fcef8a901c26a4a4ca0bd97952f4821edf357528
oselmo <opselmo@gmail.com>
2026-05-20 17:32:15 -0400

    added iam resource and updated launch_template to fix bugs and use iam resource

---
+++ modules/web-cluster/iam.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "instance_role" {
+  name = "${var.environment}-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.environment}-instance-profile"
+  role = aws_iam_role.instance_role.name
+}
--- modules/web-cluster/launch_template.tf
+++ modules/web-cluster/launch_template.tf
@@ -9,6 +9,10 @@ resource "aws_launch_template" "app" {
     security_groups             = [aws_security_group.instance_sg.id]
   }
 
+  iam_instance_profile {
+  name = aws_iam_instance_profile.instance_profile.name
+  }
+
   user_data = base64encode(<<-EOF
         #!/bin/bash
         export APP_ENV="${var.environment}"
@@ -22,8 +26,12 @@ resource "aws_launch_template" "app" {
         yum install -y httpd wget unzip
         
         # Install NodeJS
-        curl -sL https://rpm.nodesource.com/setup_16.x | bash -
-        yum install -y nodejs
+        export HOME=/root
+        export NVM_DIR="/root/.nvm"
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+        source "$NVM_DIR/nvm.sh"
+        nvm install 16.20.2
+        nvm use 16.20.2
 
         # Verify installation
         node -v
@@ -31,8 +39,8 @@ resource "aws_launch_template" "app" {
 
         # Install Ruby and Bundler
         amazon-linux-extras install -y ruby2.6
+        yum install -y ruby-devel
         gem install bundler -v 2.3.27
-
 
         # Verify installation
         ruby --version
@@ -43,23 +51,24 @@ resource "aws_launch_template" "app" {
         systemctl enable httpd
 
         # Clone the repo
-        git clone https://github.com/oselmo/terraform-concepts-project /opt/app
+        git clone https://github.com/oselmo/terraform-concepts-project /opt/repo
 
         # Install Ruby deps
-        cd /opt/app/app/backend
+        cd /opt/repo/app/backend
         bundle install
 
         # build React
-        cd /opt/app/app/frontend
+        cd /opt/repo/app/frontend
+        npm install
         npm run build
-        # outputs static files to /opt/app/app/frontend/dist
+        # outputs static files to /opt/repo/app/frontend/dist
 
         # Start Sinatra in background
-        cd /opt/app/app/backend
+        cd /opt/repo/app/backend
         ruby server.rb &
 
         # Point Apache at the React build
-        cp -r /opt/app/app/frontend/dist/* /var/www/html/
+        cp -r /opt/repo/app/frontend/dist/* /var/www/html/
 
         # configure Apache
         echo "ProxyPass /api http://localhost:4567/api
